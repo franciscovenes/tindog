efc2dd9acaf43209263c914f5fe1aca97e8f5f29
Pancho Venes <francisco.venes@gmail.com>
2023-06-19 20:39:42 +0100

    v1.1

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const endSwipe = () => {
-  document.getElementById("profile").innerHTML = `
+  document.querySelector("main").innerHTML = `
       <div class="end">
           <img src="/images/paw-icon.png" alt="">
           <p>No more profiles nearby...</p>
